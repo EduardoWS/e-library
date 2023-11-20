ba7217fa9f3637b38d2131abda8789f4c8905ec4
EduardoWS <eduardorr.ws@gmail.com>
2023-11-19 22:41:41 -0300

    att

--- widgets/WindowSlideBooks.py
+++ widgets/WindowSlideBooks.py
@@ -9,55 +9,56 @@ class WindowSlideBooks():
     def __init__(self, parent):
 
         # layout
-        parent.columnconfigure((0,1,2,3,4,5,6,7,8,9), weight=1,)
+        parent.columnconfigure(0, weight=1, uniform='a')
         parent.rowconfigure((0,1,2,3,4,5,6,7,8,9), weight=1, uniform='a')
+        
+        # fonts
+        font = ctk.CTkFont(family=FONT, size=24, weight='bold')
+        font_buttons = ctk.CTkFont(family=FONT, size=24, weight='bold')
         
         
         # frames
-        main_frame = ctk.CTkFrame(parent, corner_radius=20, fg_color='transparent')
-        main_frame.grid(column=0, row=0, sticky='nsew', pady=0, padx=10)
+        main_frame = ctk.CTkFrame(parent, fg_color='transparent')
+        main_frame.grid(column=0, row=0, rowspan=10, sticky='nsew', pady=0, padx=0)
         
-        main_frame.columnconfigure(0, weight=8, uniform='b')
-        main_frame.columnconfigure(1, weight=1, uniform='b')
+        main_frame.columnconfigure(0, weight=1, uniform='b')
         main_frame.rowconfigure(0, weight=1, uniform='b')
+        main_frame.rowconfigure(1, weight=9, uniform='b')
+        main_frame.rowconfigure(2, weight=1, uniform='b')
         
-        # back_frame = ctk.CTkFrame(main_frame, corner_radius=100, fg_color='transparent')
-        # back_frame.grid(column=0, row=0, sticky='ns', pady=0)
+        top_frame = ctk.CTkFrame(main_frame, fg_color=LIGHT_BLUE, corner_radius=20)
+        top_frame.grid(column=0, row=0, sticky='nsew', pady=5, padx=5)
         
-        font_buttons = ctk.CTkFont(family=FONT, size=24, weight='bold')
-        frame_books = ctk.CTkFrame(master=parent, fg_color='transparent', corner_radius=20)
-        frame_books.grid(column=0, row=2, rowspan=7, sticky='nswe', padx=15)
-        
-        frame_add_book = ctk.CTkFrame(master=parent, fg_color='transparent', corner_radius=20)
-        frame_add_book.grid(column=0, row=9, sticky='ns', padx=15, pady=10)
-        
-
-
-
 
         # text top
-        font = ctk.CTkFont(family=FONT, size=INPUT_FONT_SIZE, weight='bold')
-        text_top = ctk.CTkLabel(master=main_frame, text='Meus Livros', text_color=BLACK, font=font,
-                                fg_color=LIGHT_BLUE,
+        text_top = ctk.CTkLabel(master=top_frame, text='Meus Livros', text_color=BLACK, font=font,
+                                fg_color='transparent',
                                  corner_radius=20)
-        text_top.grid(column=0, row=0, sticky='nsew', pady=0, padx=5)
+        # text_top.grid(column=0,row=0, sticky='new', pady=0, padx=0)
+        text_top.place(relx=0.5, rely=0.5, anchor='center')
+        
         
         # back button
-        voltar = ctk.CTkButton(master=main_frame, text='>>', 
-                                command= parent.animate,
-                                fg_color=LIGHT_BLUE,
-                                text_color=BLACK,
-                                font=font_buttons,
-                                corner_radius=100)
+        # voltar = ctk.CTkButton(master=main_frame, text='>>', 
+        #                         command= parent.animate,
+        #                         fg_color=LIGHT_BLUE,
+        #                         text_color=BLACK,
+        #                         font=font_buttons,
+        #                         width=5, height=20,
+        #                         corner_radius=20)
         # voltar.pack(padx=5, pady=0, expand=True, fill='both')
-        voltar.grid(column=1, row=0, sticky='nsew', pady=0, padx=5)
+        voltar = ctk.CTkLabel(master=top_frame, text='>>', text_color=BLACK, font=font,
+                                fg_color='transparent',
+                                 corner_radius=20)
+        # voltar.grid(column=0, row=0, sticky='nw', pady=0, padx=10)
+        voltar.place(relx=0.05, rely=0.5, anchor='center')
         
         # add book button
         from .WindowAddBooks import WindowAddBooks
-        add_book = ctk.CTkButton(master=frame_add_book, text='+', 
+        add_book = ctk.CTkButton(master=main_frame, text='+', 
                                 command=WindowAddBooks,
                                 fg_color=LIGHT_BLUE,
                                 text_color=BLACK,
                                 font=font_buttons,
                                 corner_radius=100)
-        add_book.pack(expand=True, fill='both')
+        add_book.grid(column=0, row=9, columnspan=9, sticky='s', pady=5)
